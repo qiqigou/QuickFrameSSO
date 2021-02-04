e946bd105eb159641877281593da5916c86a7706
qiqigou <2319002545@qq.com>
2021-02-04 08:38:35 +0800

    修改用户配置

--- QuickFrameSSO/TestUsers.cs
+++ QuickFrameSSO/TestUsers.cs
@@ -21,22 +21,77 @@ namespace QuickFrameSSO.Controllers
                     country = "月球"
                 };
 
-                string[] roles = { "admin", "user" };
-
                 return new List<TestUser>
                 {
                     new TestUser
                     {
-                        SubjectId = "001",
+                        SubjectId = "100",
+                        Username = "system",
+                        Password = "123123",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "system"),
+                            new Claim(JwtClaimTypes.Role,"system"),
+                            new Claim(JwtClaimTypes.GivenName, "system"),
+                            new Claim(JwtClaimTypes.FamilyName, "系统用户"),
+                            new Claim(JwtClaimTypes.Email, "system@email.com"),
+                            new Claim(JwtClaimTypes.PhoneNumber,"110"),
+                            new Claim(JwtClaimTypes.PhoneNumberVerified,"true",ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "101",
                         Username = "admin",
                         Password = "123123",
                         Claims =
                         {
                             new Claim(JwtClaimTypes.Name, "admin"),
-                            new Claim(JwtClaimTypes.Role,JsonSerializer.Serialize(roles),IdentityServerConstants.ClaimValueTypes.Json),
+                            new Claim(JwtClaimTypes.Role,"admin"),
                             new Claim(JwtClaimTypes.GivenName, "admin"),
                             new Claim(JwtClaimTypes.FamilyName, "管理员"),
                             new Claim(JwtClaimTypes.Email, "admin@email.com"),
+                            new Claim(JwtClaimTypes.PhoneNumber,"110"),
+                            new Claim(JwtClaimTypes.PhoneNumberVerified,"true",ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "102",
+                        Username = "wyl",
+                        Password = "123123",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "wyl"),
+                            new Claim(JwtClaimTypes.Role,JsonSerializer.Serialize(new[]{ "admin", "user" }),IdentityServerConstants.ClaimValueTypes.Json),
+                            new Claim(JwtClaimTypes.GivenName, "wangyulin"),
+                            new Claim(JwtClaimTypes.FamilyName, "王玉林"),
+                            new Claim(JwtClaimTypes.Email, "wyl@email.com"),
+                            new Claim(JwtClaimTypes.PhoneNumber,"110"),
+                            new Claim(JwtClaimTypes.PhoneNumberVerified,"true",ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "103",
+                        Username = "qiqigou",
+                        Password = "123123",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "qiqigou"),
+                            new Claim(JwtClaimTypes.Role,"user"),
+                            new Claim(JwtClaimTypes.GivenName, "qiqigou"),
+                            new Claim(JwtClaimTypes.FamilyName, "qiqigou"),
+                            new Claim(JwtClaimTypes.Email, "qiqigou@email.com"),
                             new Claim(JwtClaimTypes.PhoneNumber,"110"),
                             new Claim(JwtClaimTypes.PhoneNumberVerified,"true",ClaimValueTypes.Boolean),
                             new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
